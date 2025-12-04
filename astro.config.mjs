77e44dfc167e5424748c2fbdf6e2f3427f82959c
Reuxbite <abhishek.vengurlekar.btech2025@atlasskilltech.university>
2025-12-04 18:56:41 +0530

    Add Project Nirv homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://reuxbite.github.io',
-  base: '/Project-Nirv',
+  site: 'https://projectnirv.studio',
+  base: '/',
 });
